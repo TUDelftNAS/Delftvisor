8fa032ca608aaeffddcf0e461e1d74d39a16996a
Harmjan Treep <mail@harmjantreep.nl>
2016-08-26 12:30:58 +0200

    Updated topology render script

--- render-topology.sh
+++ render-topology.sh
@@ -1,4 +1,9 @@
+cat topo.dot;
+dot -Tpng topo.dot -o topo.png;
+
+xdg-open topo.png&
+
 while inotifywait -e close_write topo.dot; do
 	cat topo.dot;
-	dot -Tpng topo.dot -O;
+	dot -Tpng topo.dot -o topo.png;
 done
